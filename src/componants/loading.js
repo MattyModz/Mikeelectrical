1d593792e2866d2f898e71661d9f56930699f5e3
MattyModz <63148923+MattyModz@users.noreply.github.com>
2022-09-02 22:23:55 +0100

    CTA URL updates

--- src/componants/loading.js
+++ src/componants/loading.js
@@ -1,15 +1,6 @@
 import { motion } from "framer-motion";
 import React from "react";
 import styles from "../styles/Loading.module.scss";
-const OVERLAY_STYLES = {
-  position: "fixed",
-  top: 0,
-  left: 0,
-  right: 0,
-  bottom: 0,
-  padding: "0px",
-  zIndex: 1000,
-};
 
 export default function Loading(props) {
   return (
@@ -22,11 +13,9 @@ export default function Loading(props) {
     >
       <div className="w-full h-full bg-logo-pink grid items-center">
         <div className="flex justify-center grid items-center ">
-          <a href="/">
-            <div className="pulse">
-              <img src="/logo.svg" height={300} width={300} />
-            </div>
-          </a>
+          <Link href="/">
+            <img src="/logo.svg" height={300} width={300} className="pulse" />
+          </Link>
         </div>
       </div>
     </motion.div>
