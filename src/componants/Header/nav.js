2db3477c73229c45cffd7cda907da084dcfa7be7
MattyModz <63148923+MattyModz@users.noreply.github.com>
2022-09-28 19:10:25 +0100

    section 2 height

--- src/componants/Header/nav.js
+++ src/componants/Header/nav.js
@@ -1,22 +1,22 @@
 import Image from "next/image";
-
+import Link from "next/link";
 export default function Nav() {
   return (
     <>
       <header className="relative py-4 md:py-6 bg-gray-50">
         <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="relative flex items-center justify-between">
-            <div className="flex-shrink-0">
-              <a
+            <div className="flex-shrink-0 mt-5">
+              <Link
                 href="/"
                 title=""
                 className="flex rounded outline-none focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"
               >
-                <Image width={150} height={75} src="/logo.svg" alt="" />
-              </a>
+                <Image width={200} height={100} src="/logo.svg" alt="" />
+              </Link>
             </div>
 
-            <div className="flex lg:hidden">
+            {/* <div className="flex lg:hidden">
               <button type="button" className="text-gray-900">
                 <svg
                   className="w-7 h-7"
@@ -33,35 +33,25 @@ export default function Nav() {
                   ></path>
                 </svg>
               </button>
-            </div>
+            </div> */}
 
-            <div className="hidden lg:absolute lg:inset-y-0 lg:flex lg:items-center lg:justify-center lg:space-x-12 lg:-translate-x-1/2 lg:left-1/2">
+            <div className=" hidden lg:absolute lg:inset-y-0 lg:flex lg:items-center lg:justify-center lg:space-x-12 lg:-translate-x-1/2 lg:left-1/2">
               <a
                 href="#"
                 title=""
-                className="text-base font-medium text-gray-900 transition-all duration-200 rounded focus:outline-none font-pj hover:text-opacity-50 focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"
+                className="text-sxl font-bold text-gray-900 transition-all duration-200 rounded focus:outline-none font-pj hover:text-opacity-50 focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"
               >
                 {" "}
                 Contact{" "}
               </a>
-
-              <a
-                href="#"
-                title=""
-                className="text-base font-medium text-gray-900 transition-all duration-200 rounded focus:outline-none font-pj hover:text-opacity-50 focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"
-              >
-                {" "}
-                Our Work{" "}
-              </a>
-
-              <a
-                href="#"
-                title=""
-                className="text-base font-medium text-gray-900 transition-all duration-200 rounded focus:outline-none font-pj hover:text-opacity-50 focus:ring-1 focus:ring-gray-900 focus:ring-offset-2"
-              >
-                {" "}
-                Blog{" "}
-              </a>
+            </div>
+            <div>
+              {" "}
+              <div className="flex justify-center p-6 mt-4  bg-gray-50/20 rounded-xl">
+                <Image src={"/IPAF.png"} width={150} height={75} />
+                <Image src={"/Nic cert.svg"} width={150} height={75} />
+                <Image src={"/logoicon.svg"} width={80} height={75} />
+              </div>
             </div>
           </div>
         </div>
