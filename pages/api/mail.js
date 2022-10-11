fa0d9697d4d65d469e2c644c078a45fa1476ddff
MattyModz <63148923+MattyModz@users.noreply.github.com>
2022-10-11 09:37:45 +0100

    add after interactive hook

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -5,7 +5,6 @@ export default async function handler(req, res) {
 
   const transporter = nodemailer.createTransport({
     service: "hotmail",
-
     auth: {
       user: process.env.user,
       pass: process.env.pass,
