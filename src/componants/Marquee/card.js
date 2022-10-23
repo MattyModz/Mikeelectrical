94bda6919892413f90b4a09939370d4f24e024c2
MattyModz <63148923+MattyModz@users.noreply.github.com>
2022-10-23 20:23:34 +0100

    styling updates for blog categorys

--- src/componants/Marquee/card.js
+++ src/componants/Marquee/card.js
@@ -1,15 +1,20 @@
-// import {
+import Image from "next/image";
+import { urlFor } from "../../../sanity";
 
-//   urlFor,
-
-// } from "../../sanity";
-export default function Reviewcard({ stars, title, review, name }) {
+export default function Reviewcard({ stars, title, review, name, img }) {
   return (
-    <div className="w-full max-w-sm mx-10 px-6 py-8 mb-8  mx-auto bg-white  rounded-xl shadow-md ">
+    <div className="w-full max-w-sm mx-10 px-6  mb-8  mx-auto bg-white  rounded-xl shadow-md ">
       <div>
         <div className="flex items-center border-b relative border-gray-100 border-solid pb-5 mb-5">
+          <Image
+            className="rounded-full mr-3 w-12 h-12"
+            src={urlFor(img).url()}
+            alt=""
+            width={50}
+            height={50}
+          />
           <div className="relative">
-            <p className="font-semibold text-gray-600 leading-none my-1">
+            <p className="font-semibold text-gray-600 leading-none my-1 pl-2">
               {name}
             </p>
             <p className="font-medium text-sm text-gray-400">
@@ -24,7 +29,8 @@ export default function Reviewcard({ stars, title, review, name }) {
           </div>
         </div>
         <div className="font-bold pb-4 text-xl">{title}</div>
-        <div> {review}</div>
+        {review}
+        <br />
       </div>
 
       <div>
