198c246b9dbaf74739864cef25c81832308961a1
MattyModz <63148923+MattyModz@users.noreply.github.com>
2022-11-15 21:21:37 +0000

    url updates

--- pages/_app.js
+++ pages/_app.js
@@ -2,26 +2,13 @@ import "../src/styles/index.scss";
 import { AnimatePresence } from "framer-motion";
 import Footer from "../src/componants/Footer/index";
 import Header from "../src/componants/Header/header";
-import { useEffect, useState } from "react";
-import Loading from "../src/componants/loading";
+
 function MyApp({ Component, pageProps, router }) {
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setLoading(false);
-  //   }, 1000);
-  // }, []);
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    });
-  }, []);
   return (
     <div>
       <Header />
 
       <AnimatePresence exitBeforeEnter>
-        <Loading loading={loading} />
         <Component {...pageProps} key={router.route} />
       </AnimatePresence>
       <Footer />
