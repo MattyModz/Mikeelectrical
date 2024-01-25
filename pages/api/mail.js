eaeb05039f01de50cf6292b3880e44262fd689e7
MattyWynne <matt@starbots-creative.co.uk>
2024-01-25 19:59:25 +0000

    touchups

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -42,5 +42,5 @@ export default async function handler(req, res) {
 
   console.log("Entered the serverless function");
 
-  return { dummy: "data" };
+  return res.status(200).json("Email sent successfully");
 }
