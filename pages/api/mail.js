f8c12de7ea4a0e28a19481a1709b9e8df67d962c
MattyWynne <matt@starbots-creative.co.uk>
2024-01-25 19:39:10 +0000

    j

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -1,42 +1,46 @@
 import nodemailer from "nodemailer";
 
 export default async function handler(req, res) {
-  const { fullName, Message, SenderEmail } = req.body;
+  // const { fullName, Message, SenderEmail } = req.body;
 
-  const transporter = nodemailer.createTransport({
-    host: "smtp-mail.outlook.com", // hostname
+  // const transporter = nodemailer.createTransport({
+  //   host: "smtp-mail.outlook.com", // hostname
 
-    secure: false,
-    port: 587, // port for secure SMTP
-    auth: {
-      user: "mikewire@hotmail.com",
-      pass: "20Birleystreet!",
-    },
-    requireTLS: true,
-    tls: {
-      ciphers: "SSLv3",
-      rejectUnauthorized: false,
-    },
-    debug: true,
-    logger: true,
-  });
+  //   secure: false,
+  //   port: 587, // port for secure SMTP
+  //   auth: {
+  //     user: "mikewire@hotmail.com",
+  //     pass: "20Birleystreet!",
+  //   },
+  //   requireTLS: true,
+  //   tls: {
+  //     ciphers: "SSLv3",
+  //     rejectUnauthorized: false,
+  //   },
+  //   debug: true,
+  //   logger: true,
+  // });
 
-  try {
-    const emailRes = await transporter.sendMail({
-      from: "mikewire@hotmail.com",
-      to: "mikewire@hotmail.com",
-      subject: `Contact form submission from ${fullName}`,
-      html: `<p>You have a new contact form submission</p><br>
-       <p><strong>Email: </strong> ${SenderEmail} </p><br>
-      <p><strong>Name: </strong> ${fullName} </p><br>
-   
-       <p><strong>Message: </strong> ${Message} </p><br>
-       `,
-    });
-    console.log("Message Sent", emailRes.messageId);
-    res.status(200).json({ message: "Email sent successfully" });
-  } catch (err) {
-    console.error("Error sending email:", err);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
+  // try {
+  //   const emailRes = await transporter.sendMail({
+  //     from: "mikewire@hotmail.com",
+  //     to: "mikewire@hotmail.com",
+  //     subject: `Contact form submission from ${fullName}`,
+  //     html: `<p>You have a new contact form submission</p><br>
+  //      <p><strong>Email: </strong> ${SenderEmail} </p><br>
+  //     <p><strong>Name: </strong> ${fullName} </p><br>
+
+  //      <p><strong>Message: </strong> ${Message} </p><br>
+  //      `,
+  //   });
+  //   console.log("Message Sent", emailRes.messageId);
+  //   res.status(200).json({ message: "Email sent successfully" });
+  // } catch (err) {
+  //   console.error("Error sending email:", err);
+  //   res.status(500).json({ error: "Internal Server Error" });
+  // }
+
+  console.log("Entered the serverless function");
+
+  return { dummy: "data" };
 }
