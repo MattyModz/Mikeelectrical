8e3c4805d96bc02fad078910c1166e7fc9cc6c0e
MattyWynne <matt@starbots-creative.co.uk>
2024-01-26 21:12:33 +0000

    update node

--- pages/api/sendmail.js
+++ pages/api/sendmail.js
@@ -1,59 +1,32 @@
 import nodemailer from "nodemailer";
 
-export default async (req, res) => {
+export default async function handler(req, res) {
   const { fullName, Message, SenderEmail } = req.body;
 
   const transporter = nodemailer.createTransport({
-    host: "smtp.office365.com", // hostname
-    // TLS requires secureConnection to be false
-    secure: false,
-    port: 587, // port for secure SMTP
-    requireTLS: true,
-    tls: {
-      ciphers: "SSLv3",
-    },
+    service: "gmail",
     auth: {
-      user: process.env.user,
-      pass: process.env.pass,
+      user: process.env.gmailUser,
+      pass: process.env.gmailPass,
     },
   });
 
-  await new Promise((resolve, reject) => {
-    // verify connection configuration
-    transporter.verify(function (error, success) {
-      if (error) {
-        console.log(error);
-        reject(error);
-      } else {
-        console.log("Server is ready to take our messages");
-        resolve(success);
-      }
+  try {
+    const emailRes = await transporter.sendMail({
+      from: "mikewiremail@gmail.com", // Replace with your Gmail address
+      to: "mikewiremail@gmail.com", // Replace with the recipient email address
+      subject: `Contact form submission from ${fullName}`,
+      html: `<p>You have a new contact form submission</p><br>
+             <p><strong>Email: </strong> ${SenderEmail} </p><br>
+             <p><strong>Name: </strong> ${fullName} </p><br>
+             <p><strong>Message: </strong> ${Message} </p><br>
+            `,
     });
-  });
 
-  const mailData = {
-    from: "mikewire@hotmail.com",
-    to: "mikewire@hotmail.com",
-    subject: `Contact form submission from ${fullName}`,
-    html: `<p>You have a new contact form submission</p><br>
-       <p><strong>Email: </strong> ${SenderEmail} </p><br>
-      <p><strong>Name: </strong> ${fullName} </p><br>
-       <p><strong>Message: </strong> ${Message} </p><br>
-       `,
-  };
-
-  await new Promise((resolve, reject) => {
-    // send mail
-    transporter.sendMail(mailData, (err, info) => {
-      if (err) {
-        console.error(err);
-        reject(err);
-      } else {
-        console.log(info);
-        resolve(info);
-      }
-    });
-  });
-
-  res.status(200).json({ status: "OK" });
-};
+    console.log("Message Sent", emailRes.messageId);
+    res.status(200).json({ message: "Email sent successfully" });
+  } catch (err) {
+    console.error("Error sending email:", err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
